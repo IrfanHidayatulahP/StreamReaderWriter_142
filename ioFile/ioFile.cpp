a5db90a1be72f878d93740a26a0e00212d08de87
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-06-19 11:36:46 +0700

    menginput data

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -19,6 +19,8 @@ int main() {
 		getline(cin, baris);
 		//loop akan berhenti jika anda memasukkan karakater q
 		if (baris == "q") break;
+		//menulis dan memasukkan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
 
 
 }
