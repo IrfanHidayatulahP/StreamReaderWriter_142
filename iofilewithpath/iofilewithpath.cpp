27f503fb2401ac1211b43ff1c9a45c86f46125d4
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-06-19 11:41:10 +0700

    membuat program perulangan

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -14,3 +14,15 @@ int main() {
 	ofstream outfile;
 	//menunjuk ke sebuah nama file
 	outfile.open(NamaFile, ios::out);
+
+	cout << ">= Menulis file, \'q'\ untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "-";
+		//mendapatkan setiap karakter dalam suatu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukkan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukkan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
