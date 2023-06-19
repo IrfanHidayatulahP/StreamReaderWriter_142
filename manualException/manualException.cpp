6f95284695ed6d618117a6ce0cd90838a3a8abd9
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-06-19 11:33:53 +0700

    Add project files.

---
+++ manualException/manualException.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	try {
+		cout << "Selamat Belajar di Prodi TI UMY" << endl;
+		//thow 0.5 //melemparkan sebuah integer maka
+	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "Peryataan tidak akan dieksekusi" << endl;
+	}
+	catch (...) {
+		/*Jika selain integer maka block ini akan dieksekusi*/
+		cout << "Default Pengecualian dixsekusi" << endl;
+	}
+
+	return 0;
+}
