86974433c90f272d3fab3977efe4b67a4233f338
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-06-19 11:42:07 +0700

    membuat program untuk menutup file

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -34,3 +34,14 @@ int main() {
 	ifstream infile;
 	//menunjuk ke sebuah file
 	infile.open(NamaFile, ios::in);
+
+	cout << endl << ">= Membuka dan membaca file" << endl;
+	//jika file ada maka
+	if (infile.is_open()) {
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris)) {
+			//dan tampilkan di sini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
